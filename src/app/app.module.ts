7ff793ee5f3202336df4588c8ce94a1a1b3c4f88
gustavo.gavancho_dev <gustavo.gavancho@devsu.com>
2023-11-14 09:08:32 -0500

    New pages and component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { SiteHeaderComponent } from './components/site-header/site-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CatalogComponent,
+    SiteHeaderComponent
   ],
   imports: [
     BrowserModule
